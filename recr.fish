b726933f78f22d307d46ed486dd93e3069fb20d6
Panoplos <dallas.johnston@gmail.com>
2017-04-29 22:47:50 +0900

    Refactored renr and added recr...

---
+++ recr.fish
@@ -0,0 +1,26 @@
+function recr -d "Rename all files in dir (default: CWD), recursively, using sed regex search & replace pattern."
+  set idx 1
+	set goback 0
+  if test (count $argv) -gt 1
+    #echo First argument $dir is a directory, pushing onto stack...
+		pushd $argv[1]
+    set idx (math "$idx + 1") > /dev/null
+		set goback 1
+  end
+  for f in *
+    if test -d $f
+			#echo $f is a directory... pushing onto stack and calling recursively!
+      pushd $f
+      recr $argv[$idx..-1]
+			echo Popping (pwd) from the stack...
+      popd
+    else
+      #echo moving $f with regex $argv[$idx]
+			command mv $f (echo $f | sed -E $argv[$idx])
+    end
+  end
+	if test $goback -eq 1
+		#echo Popping (pwd) from the stack...
+		popd
+	end
+end
--- renr.fish
+++ renr.fish
@@ -1,11 +1,23 @@
 function renr -d "Rename all files in dir (default: CWD) using sed regex search & replace (defailt: '') pattern."
-  set dir .
   set idx 1
-  if math (count $argv) "> 1" > /dev/null
-    set dir $argv[1]
-    set idx (math "$idx + 1") > /dev/null
+  set goback 0
+  if test (count $argv) -gt 1
+    if test -d $argv[1]
+      #echo Pushing the directory $argv[1]...
+      pushd $argv[1]
+      set idx (math "$idx + 1") > /dev/null
+      set goback 1
+    else
+      #echo The dir you specified either does not exist or is not a directory.
+      return -1
+    end
   end
-  for f in (ls $dir)
+  for f in *
+    #echo Moving $f with regex $argv[$idx]
     command mv $f (echo $f | sed -E $argv[$idx])
+  end
+  if test $goback -eq 1
+    #echo Popping current directory...
+    popd
   end
 end
